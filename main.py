0d1586d9c954e7d1e956a6a2282bdf0389ade0ce
Hunter <hunteralansmith@outlook.com>
2024-12-01 21:30:17 -0800

    Updated comments and docstrings. Moved some addtional files around

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ api_key = os.environ.get('GEMINI_API_KEY')
 if not api_key:
     api_key = ""
 scg = SecureCodeGen(api_key)
-response = scg.generate("Write a python application that simulates a login procedure using SSL and SNMP and store user data in a sql database. All logic for the SSL, SNMP, and SQL should be implemented")
-
-# print(response)
+scg.generate("Write a python application that simulates a login procedure using SSL and SNMP and store user data in a sql database. All logic for the SSL, SNMP, and SQL should be implemented")
